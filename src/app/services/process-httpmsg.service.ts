0a4abc78b611707aaf366d1b6f39f7cadb8f1db4
DQ <dengqian@autohome.com.cn>
2018-03-01 00:00:26 +0800

    HTTP Part 2

--- src/app/services/process-httpmsg.service.ts
+++ src/app/services/process-httpmsg.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Http, Response } from '@angular/http';
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class ProcessHTTPMsgService {
@@ -13,4 +14,17 @@ export class ProcessHTTPMsgService {
     return body || { };
   }
 
+  public handleError(error: Response | any ){
+    let errMsg: string;
+
+    if (error instanceof Response) {
+      const body = error.json() ||  '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText ||  ''} ${err}`
+    } else {
+      errMsg = error.message ? error.message: error.toString();
+    }
+
+    return  Observable.throw(errMsg);
+  }
 }
